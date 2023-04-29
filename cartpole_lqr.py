491d4abe2a05400f7aed555f628008c8e4a6ee75
UltramarineW <2021113679@stu.hit.edu.cn>
2023-04-29 15:17:07 +0800

    Add LQR cart pole

--- cartpole_lqr.py
+++ cartpole_lqr.py
@@ -20,6 +20,8 @@ from pydrake.systems.primitives import ConstantVectorSource, LogVectorOutput
 from pydrake.systems.controllers import PidController
 from pydrake.systems.controllers import LinearQuadraticRegulator
 from pydrake.systems.drawing import plot_system_graphviz
+from pydrake.systems.framework import GenerateHtml
+
 
 cart_mass = 10.0
 pole_mass = 1.0
@@ -34,57 +36,10 @@ class LQRController(LeafSystem):
         )
         self.DeclareStateOutputPort("x", state_index)
 
-        self.A = np.array([[]])
-        self.B =
-        self.Q = np.diag([[100]])
-
-
-
     def DoCalcTimeDerivatives(self, context, derivatives):
         x = context.get_state_vector().GetAtIndex(0)
         u = -self.K @ x
 
-'''
-Change the order of state to [cart_x, cart_x_dot, pole_theta, pole_theta_dot]
-'''
-class StateDataBus(LeafSystem):
-    def __init__(self):
-        LeafSystem.__init__(self)
-        input_port = self.DeclareVectorInputPort(
-            name="state_input",
-            size=4
-        )
-
-        def output(context, output):
-            x = input_port.Eval(context)
-            y = np.array([x[0], x[2], x[1], x[3]], dtype=float)
-            output.set_value(y)
-
-        output_port = self.DeclareVectorOutputPort(
-            name="output",
-            size=4,
-            calc=output
-        )
-
-
-class DataBus(LeafSystem):
-    def __init__(self, num_in, num_out):
-        LeafSystem.__init__(self)
-        input_port = self.DeclareVectorInputPort(
-            name="input",
-            size=num_in
-        )
-
-        def output(context, output):
-            x = input_port.Eval(context)
-            y = np.array([x[1], x[0]], dtype=float)
-            output.set_value(y)
-
-        output_port = self.DeclareVectorOutputPort(
-            name="output",
-            size=num_out,
-            calc=output
-        )
 
 def create_scene(meshcat, model_location, sim_time_step):
     meshcat.Delete()
@@ -99,65 +54,37 @@ def create_scene(meshcat, model_location, sim_time_step):
 
     plant.mutable_gravity_field().set_gravity_vector([0, 0, -9.8])
     plant.Finalize()
-    assert plant.geometry_source_is_registered()
+    # assert plant.geometry_source_is_registred()
+    context = plant.CreateDefaultContext()
+    # # plant state_vector stands for [x_cart, x_pole, theta_pole, theta_dot_pole]
+    eps_ = np.pi / 3
+    joint_0 = plant.GetJointByName('CartSlider')
+    joint_0.set_default_translation(0.0)
+    joint_1 = plant.GetJointByName('PolePin')
+    joint_1.set_default_angle(np.pi - eps_)
+
+    plant.get_actuation_input_port().FixValue(context, [0])
+    context.get_mutable_continuous_state_vector().SetFromVector((0, np.pi , 0, 0))
 
     visualizer = MeshcatVisualizer.AddToBuilder(
         builder, scene_graph, meshcat, MeshcatVisualizerParams(role=Role.kPerception, prefix="visual")
     )
 
-    kp = np.array([0, 500], dtype=float)
-    kd = np.array([0, 100], dtype=float)
-    ki = np.array([0, 0], dtype=float)
 
-    desired_state = ConstantVectorSource(np.array([0, np.pi, 0, 0], dtype=float))
+    pid_desired_state = ConstantVectorSource(np.array([0, np.pi, 0, 0], dtype=float))
 
-    # pid_controller = builder.AddSystem(PidController(kp, ki, kd))
-    # constant_controller = builder.AddSystem(ConstantVectorSource(np.array([0, 0])))
-    lqr_controller = builder.AddSystem(LQRController())
 
-    desired_trajectory = builder.AddSystem(desired_state)
+    Q = np.diag([1000, 10000., 0., 0.])
+    R = np.array([[1]])
+    lqr_default_controller = builder.AddSystem(LinearQuadraticRegulator(plant, context, Q, R, input_port_index=plant.get_actuation_input_port().get_index()))
 
-    ''' 
-    # PID controller
-    data_bus = custom_leaf_system(2, 2)
-    builder.AddSystem(data_bus)
-
-    builder.Connect(
-        desired_trajectory.get_output_port(),
-        pid_controller.get_input_port_desired_state()
-    )
-
+    # LQR default controller
     builder.Connect(
         plant.get_state_output_port(),
-        pid_controller.get_input_port_estimated_state()
-    )
-
-    builder.Connect(
-        pid_controller.get_output_port_control(),
-        data_bus.get_input_port()
-    )
-
-    builder.Connect(
-        data_bus.get_output_port(),
-        plant.get_actuation_input_port()
-    )
-    '''
-
-    '''
-    # ConstantController
-    builder.Connect(
-        constant_controller.get_output_port(),
-        plant.get_actuation_input_port()
-    )
-    '''
-
-    # LQR controller
-    builder.Connect(
-        plant.get_state_output_port(),
-        lqr_controller.get_input_port()
+        lqr_default_controller.get_input_port()
     )
     builder.Connect(
-        lqr_controller.get_output_port(),
+        lqr_default_controller.get_output_port(),
         plant.get_actuation_input_port()
     )
 
@@ -165,46 +92,36 @@ def create_scene(meshcat, model_location, sim_time_step):
     logger.set_name("logger")
 
     diagram = builder.Build()
-
-
-    context = diagram.CreateDefaultContext()
-
-    # plant state_vector stands for [x_cart, x_pole, theta_pole, theta_dot_pole]
-    eps_ = np.pi / 10
-    joint_0 = plant.GetJointByName('CartSlider')
-    joint_0.set_default_translation(0.0)
-    joint_1 = plant.GetJointByName('PolePin')
-    joint_1.set_default_angle(np.pi - eps_)
-
     return diagram, visualizer, plant, context, logger
 
 
 def initialize_simulation(diagram):
     simulator = Simulator(diagram)
     simulator.Initialize()
-    simulator.set_target_realtime_rate(1.0)
+    simulator.set_target_realtime_rate(1.)
     return simulator
 
 
 if __name__ == "__main__":
     meshcat = StartMeshcat()
     model_location = './cartpole.sdf'
-    sim_time_step = 0.0001
+    # sim_time_step = 0.0001
+    sim_time_step = 0.
 
     diagram, visualizer, plant, context, logger = create_scene(meshcat, model_location, sim_time_step)
 
     simulator = initialize_simulation(diagram)
     visualizer.StartRecording()
-    simulator.AdvanceTo(10.0)
+    meshcat.AddButton('Stop Simulation')
+    while meshcat.GetButtonClicks('Stop Simulation') < 1:
+        simulator.AdvanceTo(simulator.get_context().get_time() + 1.0)
     visualizer.PublishRecording()
+
 
 
     state_log = logger.FindLog(simulator.get_context())
     time = state_log.sample_times()
     state_data = state_log.data()
-    print(state_data)
-    print(state_data.shape)
-    print(time)
 
     plt.figure()
     plt.plot(time, state_data[1], '.-')
